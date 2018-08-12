6f08e3b6e8a8ffe75714673646a65ce7bcd9d99d
jean <jean.ellwanger@gmail.com>
2018-08-12 10:22:07 -0300

    Add galaxA5

--- scan_network.py
+++ scan_network.py
@@ -4,7 +4,7 @@
 from subprocess import Popen,PIPE,STDOUT,call
 
 macs_ok = ["e8:89:2c:45:79:31","00:1e:58:2a:3a:9a",
-"a4:77:33:fe:77:a8","80:d2:1d:42:27:fc","54:60:09:bc:61:a0","bc:6e:64:aa:37:2c"]
+"a4:77:33:fe:77:a8","80:d2:1d:42:27:fc","54:60:09:bc:61:a0","bc:6e:64:aa:37:2c", "50:92:B9:4B:19:12"]
 
 now_in_network = "sudo arp-scan --interface=wlp2s0 --localnet | grep -o -E \'([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}\'"
 
