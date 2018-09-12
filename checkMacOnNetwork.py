5fbd0415b2e8f7191c9720db90f54eedd150acae
Jeancarlo <jeancarlo@cds.eb.mil.br>
2018-09-12 16:10:18 -0300

    Retirada de código excedente

--- checkMacOnNetwork.py
+++ checkMacOnNetwork.py
@@ -10,22 +10,6 @@ ARQUIVO = "/home/jean/diario.txt"
 STATUS_CHEGADA = "chegou"
 STATUS_SAIDA = "saiu"
 
-macs_ok = ["e8:89:2c:45:79:31","00:1e:58:2a:3a:9a",
-"a4:77:33:fe:77:a8","80:d2:1d:42:27:fc","54:60:09:bc:61:a0","bc:6e:64:aa:37:2c", "50:92:B9:4B:19:12"]
-
-def not_authorized():
-	now_in_network = "sudo arp-scan --interface=wlp2s0 --localnet | grep -o -E \'([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}\'"
-
-	proc=Popen(now_in_network, shell=True, stdout=PIPE, )
-	output=proc.communicate()[0]
-
-	now_in_network = output.split("\n")
-
-	not_authorized = list(set(now_in_network) - set(macs_ok))
-	not_authorized.remove('')
-
-	return not_authorized
-
 def check(mac_address):
 	now_in_network = "sudo arp-scan --interface=wlp2s0 --localnet | grep -o -E \'([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}\'"
 	proc=Popen(now_in_network, shell=True, stdout=PIPE, )
@@ -46,8 +30,7 @@ def bate_ponto(mac_address):
 		escreve("Vamos começar!")
 
 def escreve(status):
-	hora_agora = datetime.today()
-	hora_agora = hora_agora.strftime("%Y-%m-%d %H:%M:%S")
+	hora_agora = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 	text_file = open(ARQUIVO, "a")
 	text_file.write("\n"+hora_agora+" -"+status)
 	text_file.close()
