994cf7cb482330e6a74888b3088d41d5b4f4e8e5
jean <jean.ellwanger@gmail.com>
2018-09-12 19:25:33 -0300

    Versão nova

--- checkMacOnNetwork.py
+++ checkMacOnNetwork.py
@@ -6,37 +6,53 @@ from time import strftime
 from datetime import datetime, timedelta
 import sys
 
-ARQUIVO = "/home/jean/diario.txt"
-STATUS_CHEGADA = "chegou"
-STATUS_SAIDA = "saiu"
+DIARIO = "/home/jean/diario.txt"
+STRANGE = "home/jean/strange.txt"
 
-def check(mac_address):
+STATUS_CHEGADA = "conectou"
+STATUS_SAIDA = "desconectou"
+
+def now_on_network():
 	now_in_network = "sudo arp-scan --interface=wlp2s0 --localnet | grep -o -E \'([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}\'"
+
 	proc=Popen(now_in_network, shell=True, stdout=PIPE, )
 	output=proc.communicate()[0]
 
 	now_in_network = output.split("\n")
+	now_in_network = [x.lower() for x in now_in_network]
 
-	return mac_address in now_in_network
+	return now_in_network
+
+def not_authorized(network_now):
+	MACS_OK = ["e8:89:2c:45:79:31","00:1e:58:2a:3a:9a","a4:77:33:fe:77:a8","80:d2:1d:42:27:fc","54:60:09:bc:61:a0","bc:6e:64:aa:37:2c"]
+	MACS_OK = [x.lower() for x in MACS_OK]
+	not_authorized = list(set(network_now) - set(MACS_OK))
+	if '' in not_authorized:
+		not_authorized.remove('')
+
+	return ','.join(not_authorized)
+
+def check(mac_address, network_now):
+	return mac_address.lower() in network_now
 
 def bate_ponto(mac_address):
-	status = ultimo_status()
-	if not check(mac_address):
-		if status == STATUS_CHEGADA:
-			escreve(STATUS_SAIDA)
-	elif status == STATUS_SAIDA:
-		escreve(STATUS_CHEGADA)
-	else:
-		escreve("Vamos começar!")
+	network_now = now_on_network()
+	status = ultimo_status(DIARIO)
 
-def escreve(status):
+	if not check(mac_address, network_now):
+		if status == STATUS_CHEGADA:
+			escreve(STATUS_SAIDA, mac_address, DIARIO)
+	elif status == STATUS_SAIDA:
+		escreve(STATUS_CHEGADA, mac_address, DIARIO)
+
+def escreve(status, mac_address, arquivo):
 	hora_agora = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-	text_file = open(ARQUIVO, "a")
-	text_file.write("\n"+hora_agora+" -"+status)
+	text_file = open(arquivo, "a")
+	text_file.write("\n"+mac_address+" > "+hora_agora+" -"+status)
 	text_file.close()
 
-def ultimo_status():
-	text_file = open(ARQUIVO, "r")
+def ultimo_status(arquivo):
+	text_file = open(arquivo, "r")
 	diario = text_file.read()
 	try:
 		ultimo_status = diario.split("-")
@@ -55,4 +71,5 @@ if __name__ == '__main__':
 		sys.exit(1)
 
 	mac_address = sys.argv[1]
+
 	bate_ponto(mac_address)
